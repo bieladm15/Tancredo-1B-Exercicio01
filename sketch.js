4bdfa755d472cf7921e955bd7ba6f796865b3969
Gabriel Osmar <141455338+bieladm15@users.noreply.github.com>
2023-08-11 17:41:22 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 600);
+background( "blue")
+}
+function draw() {
+  fill( "black");
+stroke ("yellow") 
+  if(mouseIsPressed) { 
+  
+rect(mouseX,mouseY,50, 50);
+}
+}
+  
